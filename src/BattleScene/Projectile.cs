a07f03ecf012a525c2414bb073a1eec087b89e52
Ivan-Krul <kiwiivanya128@gmail.com>
2023-12-04 22:54:01 +0100

    created a Projectile class

---
+++ src/BattleScene/Projectile.cs
@@ -0,0 +1,36 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneInfection.src.BattleScene
+{
+	public class Projectile
+	{
+		public float Radius { get; private set; }
+		public Vector2 Speed { get; private set; }
+		public Vector2I Coord { get; private set; }
+
+		private bool isSpeedDecreasing = false;
+
+		public Projectile(Vector2I start, float radius, bool isSpeedDecreasing)
+		{
+			Radius = radius;
+			Coord = start;
+			this.isSpeedDecreasing = isSpeedDecreasing;
+		}
+
+		public bool IsDestroyable()
+		{
+			var res = DisplayServer.WindowGetSize();
+			return
+				(Radius < 0.1f) ||
+				((Coord.X - Radius) > res.X) ||
+				((Coord.Y - Radius) > res.Y) ||
+				(Coord.X + Radius) < 0 ||
+				(Coord.Y + Radius) < 0;
+		}
+	}
+}
